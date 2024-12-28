8fe0fc4627fd809cf0a3033e3219ba5399dfb512
OlegTerek <olegter549@gmail.com>
2024-12-28 19:25:25 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -12,9 +12,9 @@ for i in range(len(numbers)):
         elif numbers[i] % j == 0 and j != numbers[i]:
             is_prime = True
             break
-    if is_prime == True:
+    if is_prime == True and numbers[i] != 1:
         primes.append(numbers[i])
-    elif is_prime == False:
+    elif is_prime == False and numbers[i] != 1:
         not_primes.append(numbers[i])
 
 print("Not primes", not_primes)
